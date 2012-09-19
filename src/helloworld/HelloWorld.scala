ad2be489ff50441a1d7cc37d4d79da103b13a596
mguindin <matt@guindin.com>
2012-09-18 22:17:09 -0400

    [scala] first commit of repo

---
+++ src/helloworld/HelloWorld.scala
@@ -0,0 +1,15 @@
+package helloworld
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: matt
+ * Date: 9/18/12
+ * Time: 10:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+object HelloWorld{
+  def main(args: Array[String]) {
+    println("Hello, World!")
+  }
+
+}
